7ba5f6555806064db8b05d23436540981179427b
Joshua Woodward <jdw6359@rit.edu>
2016-05-02 17:28:23 -0400

    Client endpoints include related device objects

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,16 +2,20 @@ class ClientsController < ApplicationController
   before_action :set_client, only: [:show]
 
   def index
-    @clients = Client.all
-    render json: @clients      
+    @clients = Client.includes(client_associations).all
+    render json: @clients, include: client_associations
   end
 
   def show
-    render json: @client
+    render json: @client, include: client_associations
   end
 
   private
   def set_client
-    @client = Client.find(params[:id])
+    @client = Client.includes(client_associations).find(params[:id])
+  end
+
+  def client_associations
+    [:devices]
   end
 end
